d684f8ed601945e8177b3eb7a4f6aef14b70757a
JMKim <tdcian71@gmail.com>
2025-02-22 22:11:11 +0900

    meet expectation

--- InfinitePagingCarousel/ViewController.swift
+++ InfinitePagingCarousel/ViewController.swift
@@ -45,15 +45,19 @@ final class ImageCollectionViewCell: UICollectionViewCell {
 
 class ViewController: UIViewController {
     
+    private var startIndex: Int = 1
+    
     var items: [UIImage] = [
         ._1,
         ._2,
         ._3,
         ._4,
+        ._5
     ]
-
+    
     enum Metric {
         static let collectionViewHeight = 350.0
+        static let cellWidth = UIScreen.main.bounds.width
     }
     
     private lazy var carouselCollectionView: UICollectionView = {
@@ -61,16 +65,17 @@ class ViewController: UIViewController {
         layout.scrollDirection = .horizontal
         layout.minimumLineSpacing = 0
         layout.minimumInteritemSpacing = 0
-        let collectionView = UICollectionView(
-            frame: .zero,
-            collectionViewLayout: layout
-        )
+        layout.itemSize = CGSize(width: Metric.cellWidth, height: Metric.collectionViewHeight)
+        
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.layer.borderColor = UIColor.red.cgColor
+        collectionView.layer.borderWidth = 1
         collectionView.isScrollEnabled = true
         collectionView.isPagingEnabled = true
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.backgroundColor = .clear
         collectionView.dataSource = self
         collectionView.delegate = self
-        collectionView.layer.borderColor = UIColor.blue.cgColor
-        collectionView.layer.borderWidth = 1
         collectionView.register(
             ImageCollectionViewCell.self,
             forCellWithReuseIdentifier: ImageCollectionViewCell.identifier
@@ -83,60 +88,85 @@ class ViewController: UIViewController {
         pageControl.currentPageIndicatorTintColor = .systemGreen
         pageControl.pageIndicatorTintColor = .red
         pageControl.backgroundStyle = .minimal
-        pageControl.numberOfPages = 4
         pageControl.isUserInteractionEnabled = false
         return pageControl
     }()
-    
-    private let indexLabel: UILabel = {
+
+    private lazy var indexLabel: UILabel = {
         let label = UILabel()
-        label.layer.borderColor = UIColor.red.cgColor
+        label.layer.borderColor = UIColor.blue.cgColor
         label.layer.borderWidth = 1
-        label.textColor = .label
-        label.font = .systemFont(ofSize: 18, weight: .semibold)
         label.textAlignment = .center
+        label.font = .systemFont(ofSize: 17, weight: .medium)
+        label.textColor = .secondaryLabel
         return label
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        view.addSubview(carouselCollectionView)
+        view.addSubview(pageControl)
+        view.addSubview(indexLabel)
         
-        self.items.insert(self.items[items.count - 1], at: 0)
-        self.items.append(self.items[1])
-        
-        self.view.addSubview(self.carouselCollectionView)
-        self.view.addSubview(self.pageControl)
-        
-        self.carouselCollectionView.snp.makeConstraints { make in
-            make.top.equalTo(self.view.safeAreaLayoutGuide.snp.top)
-            make.horizontalEdges.equalToSuperview()
-            make.height.equalTo(Metric.collectionViewHeight)
+        carouselCollectionView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+            $0.horizontalEdges.equalToSuperview()
+            $0.height.equalTo(Metric.collectionViewHeight)
         }
         
-        self.pageControl.snp.makeConstraints { make in
-            make.bottom.equalTo(self.carouselCollectionView.snp.bottom).offset(-10)
-            make.centerX.equalToSuperview()
+        pageControl.snp.makeConstraints {
+            $0.top.equalTo(carouselCollectionView.snp.bottom).offset(15)
+            $0.centerX.equalToSuperview()
+        }
+        
+        indexLabel.snp.makeConstraints {
+            $0.top.equalTo(pageControl.snp.bottom).offset(15)
+            $0.centerX.equalToSuperview()
+            $0.width.equalTo(100)
+            $0.height.equalTo(50)
         }
 
+        items.insert(items[items.count - 1], at: 0)
+        items.append(items[1])
+        
+        pageControl.numberOfPages = self.items.count - 2
+        
+        if startIndex == 0 {
+            pageControl.currentPage = 0
+            indexLabel.text = "1/\(self.items.count - 2)"
+        } else {
+            pageControl.currentPage = startIndex
+            indexLabel.text = "\(startIndex + 1)/\(self.items.count - 2)"
+        }
     }
-
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
         
-        self.carouselCollectionView.setContentOffset(
-            .init(
-                x: self.carouselCollectionView.frame.width,
-                y: self.carouselCollectionView.contentOffset.y
-            ),
-            animated: false
-        )
+        if startIndex == 0 {
+            carouselCollectionView.setContentOffset(
+                .init(
+                    x: Metric.cellWidth,
+                    y: carouselCollectionView.contentOffset.y
+                ),
+                animated: false
+            )
+        } else {
+            carouselCollectionView.setContentOffset(
+                .init(
+                    x: Metric.cellWidth * Double(startIndex + 1),
+                    y: carouselCollectionView.contentOffset.y
+                ),
+                animated: false
+            )
+        }
     }
 }
 
 extension ViewController: UICollectionViewDataSource {
-    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return self.items.count
+        return items.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -146,57 +176,53 @@ extension ViewController: UICollectionViewDataSource {
         ) as? ImageCollectionViewCell else {
             return UICollectionViewCell()
         }
-        
-        let item = self.items[indexPath.item]
-        cell.setImage(item)
+        let image = items[indexPath.item]
+        cell.setImage(image)
         return cell
     }
 }
 
 extension ViewController: UICollectionViewDelegate {
-    
     func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
-        
+
         var page = Int(scrollView.contentOffset.x / scrollView.frame.maxX) - 1
         
-        if page == self.items.count - 2 {
+        if page == items.count - 2 {
             page = 0
         }
         
         if page == -1 {
-            page = self.items.count - 3
+            page = items.count - 3
         }
         
-        self.pageControl.currentPage = page
+        pageControl.currentPage = page
         
-        let count = self.items.count
+        DispatchQueue.main.async { [weak self] in
+            guard let self else { return }
+            self.indexLabel.text = "\(page + 1)/\(self.items.count - 2)"
+        }
         
+        let count = items.count
+        
+        print("### 페이지: \(page), 페이지 + 1: \(page + 1)")
+
         if scrollView.contentOffset.x == 0 {
             scrollView.setContentOffset(
                 .init(
-                    x: scrollView.frame.width * Double(count - 2),
+                    x: Metric.cellWidth * Double(count - 2),
                     y: scrollView.contentOffset.y
                 ),
                 animated: false
             )
         }
-        
-        if scrollView.contentOffset.x == Double(count - 1) * scrollView.frame.width {
+        if scrollView.contentOffset.x == Double(count - 1) * Metric.cellWidth {
             scrollView.setContentOffset(
                 .init(
-                    x: scrollView.frame.width,
+                    x: Metric.cellWidth,
                     y: scrollView.contentOffset.y
                 ),
                 animated: false
             )
         }
-        
-        print("### 페이지: \(page), 카운트: \(count)")
-    }
-}
-
-extension ViewController: UICollectionViewDelegateFlowLayout {
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return collectionView.frame.size
     }
 }
